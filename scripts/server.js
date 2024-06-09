e8eb738605030cf4d9abc49c33a79ffff3987168
xnmeow <anastasiatensvt@gmail.com>
2024-06-09 20:42:28 +1000

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -12,7 +12,7 @@ fetch('https://558bf9d2-8c15-400c-81af-0d7d93356735-00-1skip0tdjrou7.sisko.repli
             <img src="img/portfolio/${item.id}.jpeg" alt="">
             <div class="fog"></div>
             <div class="portfolio__item__info">
-                <h3>${item.name} ${item.cost}₽</h3>
+                <h3>${item.name}</h3>
             </div>
         </div>
         `
